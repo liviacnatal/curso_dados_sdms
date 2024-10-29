0d604856358f719ec9fe74c5f2101d62e458c9d6
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2024-10-29 12:19:58 -0300

    misc

--- sessoes/funcoes_de_apoio/instalar_pacotes.R
+++ sessoes/funcoes_de_apoio/instalar_pacotes.R
@@ -4,6 +4,9 @@ source(paste0(here(),"/sessoes/funcoes_de_apoio/fun_list.R"))
 packages <- c("tidyverse", 
               "sf", # for loading shapefiles
               "sp",
+              "marmap",
+              "cowplot",
+              "ggspatial", # For map pimping
               "tools", # for loading shapefiles
               "here", # for easy paths
               "rnaturalearth",
